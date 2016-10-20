5301a99604553f0549040519a153c00ba66b06bc
Jonathan DiLorenzo <dilorenzo@cs.cornell.edu>
2016-10-20 19:05:29 -0400

    Fixed desugar script

--- desugar.sh
+++ desugar.sh
@@ -1,7 +1,4 @@
-#OCAMLFIND="ocamlfind query -predicates syntax,preprocessor -r"
-#INCLUDE=`$OCAMLFIND -i-format forest.syntax`
-#ARCHIVES=`$OCAMLFIND -a-format forest.syntax`
-#camlp4o -printer o $INCLUDE str.cma $ARCHIVES $1
-#ocamlfind ocamlc -dsource -package forest $1
-ppxdir=$(dirname $0)
-ocamlfind ppx_tools/rewriter $ppxdir/ppx_pads.native $1
+#!/bin/bash
+
+ppxdir=$(ocamlfind query pads)/../../bin
+ocamlfind ppx_tools/rewriter $ppxdir/ppx_pads $1
